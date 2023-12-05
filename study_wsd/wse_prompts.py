44fc7e34a644faa93001895cb3deef87afc9b4b1
root <root@e-bnbnbo-55cmr-0.e-bnbnbo-55cmr.ace-hub.svc.cluster.local>
2023-12-05 04:32:10 +0000

    no updates

--- study_wsd/wse_prompts.py
+++ study_wsd/wse_prompts.py
@@ -1,4 +1,5 @@
 import typing
+import re
 
 from study_wsd.prompt import PromptFactory, Prompt
 from study_wsd.wse import WordSenseEvaluation
@@ -15,11 +16,51 @@ def find_letter_response(response:str) -> typing.Optional[str]:
     return l_response
 
 
+def find_letter_response_llama(response:str) -> typing.Optional[str]:
+    l_response = None
+
+    pattern = r'### ANSWER: \[/INST\].*?\b([A-Z])\b(?=\))'
+    match = re.search(pattern, response, re.DOTALL)
+    if match:
+        l_response = match.group(1)
+
+    return l_response
+
+
+def find_letter_response_vicuna(response:str) -> typing.Optional[str]:
+    l_response = None
+
+    # pattern = r'### ANSWER:[^)]*?([A-Z])\)'
+    pattern = r'### ANSWER:[\s\S]*?([A-Z])\)'
+    match = re.search(pattern, response)
+    if match:
+        l_response = match.group(1)
+
+    return l_response
+
+
+def find_letter_response_guanaco(response:str) -> typing.Optional[str]:
+    l_response = None
+
+    pattern = r'ANSWER:[^A-Z]*([A-Z])'
+    match = re.search(pattern, response)
+    if match:
+        l_response = match.group(1)
+
+    return l_response 
+
+
+def find_letter_response_palm(response:str) -> typing.Optional[str]:
+    l_response = response
+
+    return l_response 
+    
+
 class DefaultWsePrompt(PromptFactory[WordSenseEvaluation]):
 
     def __init__(self,
                  topic:WordSenseEvaluation,
-                 response_interpretter_function=find_letter_response
+                 response_interpretter_function=find_letter_response_vicuna
         ):
         self.topic = topic
         self.answer_value = None
@@ -70,7 +111,7 @@ class DirectWsePrompt(PromptFactory[WordSenseEvaluation]):
 
     def __init__(self,
                  topic:WordSenseEvaluation,
-                 response_interpretter_function=find_letter_response
+                 response_interpretter_function=find_letter_response_vicuna
         ):
         self.topic = topic
         self.answer_value = None
